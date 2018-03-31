082e6a9ba5a7cc7daac8d2f5fb23dd712b1dbdec
Dell <Dell@DESKTOP-G4C0CMQ>
2018-03-31 22:36:43 +0800

    Add project files.

---
+++ CustomizeKeychain/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomizeKeychain.Startup))]
+namespace CustomizeKeychain
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
